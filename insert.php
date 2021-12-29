14a7dd59d32ac85e3db64e5c4c17e2b0b97c2aa6
jayeshthk <thakarejaydi18ce@student.mes.ac.in>
2021-12-29 10:19:11 +0530

    first commit

---
+++ insert.php
@@ -0,0 +1,19 @@
+<?php
+$conn = mysqli_connect('localhost','root','','subscribe_fitclub');
+$email_user =  $_REQUEST['email'];
+$sql = "INSERT INTO email VALUES ('$email_user')";
+if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully." 
+                . " Please browse your localhost php my admin" 
+                . " to view the updated data</h3>"; 
+  
+            echo nl2br("\n$first_name\n $last_name\n "
+                . "$gender\n $address\n $email");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+          
+        // Close connection
+        mysqli_close($conn);
+?>
